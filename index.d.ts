02eaa05d18978cb77be06915d1293eb1e5efaa33
Yaroslav Dobzhanskij <merlyn.tgz@gmail.com>
2020-10-15 10:43:37 +0300

    Changed tsconfig

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,4 @@
-import { PKCEHelper } from 'abstract-pkce'
-
-export type { PKCEHelper, PKCEChallenge } from 'abstract-pkce'
-
-export declare type PKCEHelperLegacyBrowser = PKCEHelper<string>
-
-export declare const createPKCEHelper: (isHMAC?: boolean) => PKCEHelperLegacyBrowser
+import { PKCEHelper } from 'abstract-pkce';
+export type { PKCEHelper, PKCEChallenge } from 'abstract-pkce';
+export declare type PKCEHelperLegacyBrowser = PKCEHelper<string>;
+export declare const createPKCEHelper: (isHMAC?: boolean) => PKCEHelperLegacyBrowser;
